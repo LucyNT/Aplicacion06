38cae3e9bb2cb85cdd1749659189911081ba3f34
AL30F13 <luci.nt@gmail.com>
2015-04-28 13:26:08 -0500

    dos
    segunda subida

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,18 @@
 // JavaScript Document
 $(document).ready(function(e) {
-document.addEventListener("deviceready",function(){
-	
-}); 
+document.addEventListener("deviceready",function(){//cuando el dispositivo este listo
+	$('#btnvibrar').on('tap',function(){
+		navigator.vibrate(1000);
+});//tap a btnvibrar 
+$('#btnbeep').on('tap',function(){
+		navigator.beep(1);
+});//tap a btnbeep
+$('#Izquierda').on('swipe left',function(){
+		alert.("Barrido a la izquierda");
+});
+$('#Derecha').on('swipe right',function(){
+		alert.("Barrido a la derecha");
+});
+});
 });
 
