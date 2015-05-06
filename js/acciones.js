82c218743e1fc1e50debb1fca1a28d95d7674d70
AL30F13 <luci.nt@gmail.com>
2015-05-06 13:51:41 -0500

    cinco
    se suman dos eventos mas

--- js/acciones.js
+++ js/acciones.js
@@ -3,16 +3,25 @@ $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){//cuando el dispositivo este listo
 	$('#btnvibrar').on('tap',function(){
 		navigator.vibrate(1000);
-});//tap a btnvibrar 
-$('#btnbeep').on('tap',function(){
+   });//tap a btnvibrar 
+        $('#btnbeep').on('tap',function(){
 		navigator.beep(1);
-});//tap a btnbeep
-$('#Izquierda').on('swipe left',function(){
+   });//tap a btnbeep
+        $('#Izquierda').on('swipe left',function(){
 		alert.("Barrido a la izquierda");
-});
-$('#Derecha').on('swipe right',function(){
+   });
+        $('#Derecha').on('swipe right',function(){
 		alert.("Barrido a la derecha");
-});
+   });
+   document.addEventListener("pause",function(){
+         $('#listado').append("<p> la aplicacion se pauso </p>"); //agregar al final = append
+   });
+    document.addEventListener("resume",function(){
+		 $('#listado').append("<p> se reinicio la aplicacion </p>");
+   });
+   $(window).on('orientationchange',function(e){
+	$('#listado').append("<p>orientation: "+ e.orientation + "</p>");
+   });
 });
 });
 
